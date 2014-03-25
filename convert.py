c551293fd45148337c78b74fd4af22329bebcc26
epot <emmanuel.pot@gmail.com>
2014-03-25 21:20:23 +0100

    scan folder feature

--- convert.py
+++ convert.py
@@ -5,15 +5,27 @@ Created on 13 Dec 2013
 '''
 
 import os
+import argparse
 
 from converter import gpxhrmtotcx
 
+def scan(folder):
+    for filepath in os.listdir(folder):
+        filename, fileextension = os.path.splitext(filepath)
+        gpx_file = os.path.join(folder, filepath)
+        if os.path.isfile(gpx_file) and fileextension == '.gpx':  
+            hrm_file = os.path.join(folder, '{}.hrm'.format(filename))
+            tcx_file = os.path.join(folder, '{}.tcx'.format(filename))
+            if not os.path.exists(tcx_file) and os.path.exists(hrm_file):
+                converter = gpxhrmtotcx.GpxHrmToTcx(gpx_file, hrm_file)
+                print 'converting {} and {} to {}'.format(os.path.join(folder, filepath), hrm_file, tcx_file)
+                converter.convert(tcx_file)
+
 def main():
-    data_path = os.path.join(os.path.dirname(__file__), 'data')
-    gna = gpxhrmtotcx.GpxHrmToTcx(
-          os.path.join(data_path, '13121201.gpx'), 
-          os.path.join(data_path, '13121201.hrm'))
-    gna.convert(os.path.join(data_path, '13121201.tcx'))
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", "--folder", help="Folder to scan")
+    args = parser.parse_args()
+    scan(args.folder)
 
 if __name__ == '__main__':
     main()
