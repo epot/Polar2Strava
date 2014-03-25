3aeaf5843db98dece9d53bebf250368ce3712e56
epot <emmanuel.pot@gmail.com>
2014-03-25 22:09:45 +0100

    add email to strava

--- convert.py
+++ convert.py
@@ -6,10 +6,45 @@ Created on 13 Dec 2013
 
 import os
 import argparse
+import smtplib
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEBase import MIMEBase
+from email.MIMEText import MIMEText
+from email.Utils import COMMASPACE, formatdate
+from email import Encoders
 
 from converter import gpxhrmtotcx
 
-def scan(folder):
+
+def send_mail(send_from, send_to, subject, text, files=[], server="localhost"):
+    assert type(send_to)==list
+    assert type(files)==list
+
+    msg = MIMEMultipart()
+    msg['From'] = send_from
+    msg['To'] = COMMASPACE.join(send_to)
+    msg['Date'] = formatdate(localtime=True)
+    msg['Subject'] = subject
+
+    msg.attach( MIMEText(text) )
+
+    for f in files:
+        part = MIMEBase('application', "octet-stream")
+        part.set_payload( open(f,"rb").read() )
+        Encoders.encode_base64(part)
+        part.add_header('Content-Disposition', 'attachment; filename="%s"' % os.path.basename(f))
+        msg.attach(part)
+
+    smtp = smtplib.SMTP(server)
+    smtp.sendmail(send_from, send_to, msg.as_string())
+    smtp.close()
+
+def upload_to_strava(email_from, new_tcx_files):
+    if email_from and new_tcx_files:
+        send_mail(email_from, ["upload@strava.com"], "Upload", "", new_tcx_files)
+
+def scan(folder, email_from):
+    new_tcx_files = []
     for filepath in os.listdir(folder):
         filename, fileextension = os.path.splitext(filepath)
         gpx_file = os.path.join(folder, filepath)
@@ -19,13 +54,20 @@ def scan(folder):
             if not os.path.exists(tcx_file) and os.path.exists(hrm_file):
                 converter = gpxhrmtotcx.GpxHrmToTcx(gpx_file, hrm_file)
                 print 'converting {} and {} to {}'.format(os.path.join(folder, filepath), hrm_file, tcx_file)
-                converter.convert(tcx_file)
+                try:
+                    converter.convert(tcx_file)
+                    new_tcx_files.append(tcx_file)
+                except gpxhrmtotcx.GpxHrmToTcx.GpxHrmToTcxException as e:
+                    print "Conversion failed: {}".format(e)
+                
+    upload_to_strava(email_from, new_tcx_files)
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-f", "--folder", help="Folder to scan")
+    parser.add_argument("-e", "--email", help="If this is set, then an email will be sent to Strava with new results (needs a smtp server on localhost).")
     args = parser.parse_args()
-    scan(args.folder)
+    scan(args.folder, args.email)
 
 if __name__ == '__main__':
     main()
--- converter/gpxhrmtotcx.py
+++ converter/gpxhrmtotcx.py
@@ -3,7 +3,8 @@ Created on 13 Dec 2013
 
 This modules allows you to merge the content of a gpx and its associated hrm in a single tcx file.
 
-It is highly relies on http://colby.id.au/combining-gpx-and-hrm-files.
+It is highly relies on http://colby.id.au/combining-gpx-and-hrm-files
+(and therefore only works on machine with awk installed)
 
 It could be done in a much nicer/modular way (with xsl transformation) if time allows.
 
@@ -11,7 +12,6 @@ It could be done in a much nicer/modular way (with xsl transformation) if time a
 '''
 
 import subprocess
-import shlex
 import os
 
 class GpxHrmToTcx(object):
@@ -38,9 +38,6 @@ class GpxHrmToTcx(object):
             awk_file = os.path.join(os.path.dirname(__file__), 'thirdparty', 'gpx2tcx.awk')
             cmd = 'awk -f {} -v HRMFILE={} {} > {}'.format(
                     awk_file, self.__hrm_file_path,self.__gpx_file_path, os.path.abspath(tcx_file_path))
-            print "slip"
-            print cmd
-            print "slip"
             subprocess.check_call(cmd, shell=True)
                 
         except (subprocess.CalledProcessError, OSError) as e:
